26a3e8a22978d6a201f84ebfb99991f5760a8e1a
Abhina Sreeskantharajan <Abhina.Sreeskantharajan@ibm.com>
2026-08-11 08:58:50 -0400

    Add isNoop to avoid conversion code in ActOnGCCAsmStmtString

--- clang/lib/Sema/SemaStmtAsm.cpp
+++ clang/lib/Sema/SemaStmtAsm.cpp
@@ -245,7 +245,7 @@ ExprResult Sema::ActOnGCCAsmStmtString(Expr *Expr, bool ForAsmLabel) {
       Diag(Expr->getBeginLoc(), diag::err_asm_operand_empty_string)
           << SL->getSourceRange();
     }
-    if (Context.getTargetInfo().FromSystemEncodingConverter != nullptr) {
+    if (!Context.getTargetInfo().FromSystemEncodingConverter->isNoop()) {
       SmallString<16> ConvertedAsm;
       Context.getTargetInfo().FromSystemEncodingConverter->convert(
           SL->getString(), ConvertedAsm);
@@ -257,6 +257,7 @@ ExprResult Sema::ActOnGCCAsmStmtString(Expr *Expr, bool ForAsmLabel) {
     }
     return SL;
   }
+
   if (DiagnoseUnexpandedParameterPack(Expr))
     return ExprError();
   if (Expr->getDependence() != ExprDependence::None)
