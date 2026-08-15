2bcc9fbcbbabdb26ceb1707684114de2b5ac10b8
Kh05ifr4nD <meandSSH0219@gmail.com>
2026-08-15 14:56:22 +0800

    MC: initialize section fragment list

--- llvm/include/llvm/MC/MCSection.h
+++ llvm/include/llvm/MC/MCSection.h
@@ -585,7 +585,7 @@ public:
 private:
   // At parse time, this holds the fragment list of the current subsection. At
   // layout time, this holds the concatenated fragment lists of all subsections.
-  FragList *CurFragList;
+  FragList *CurFragList = nullptr;
   // In many object file formats, this denotes the section symbol. In Mach-O,
   // this denotes an optional temporary label at the section start.
   MCSymbol *Begin;
